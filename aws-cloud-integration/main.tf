2ae623066d00a4bd3b4dfb39a59dae5e833acb00
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-05-26 18:36:30 +0100

    Delete aws-cloud-integration directory

--- aws-cloud-integration/main.tf
+++
@@ -1,5 +0,0 @@
-resource "spacelift_aws_integration" "this" {
-  name                          = var.role_name
-  role_arn                      = var.role_arn
-  generate_credentials_in_worker = false
-}
--- aws-cloud-integration/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_name" {
-  description = "The name of the IAM role assumed by Spacelift"
-  type        = string
-}
-
-variable "role_arn" {
-  description = "The ARN of the IAM role assumed by Spacelift"
-  type        = string
-}
