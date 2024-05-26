6296e6f919225ff179fc81d88dbfa14faf046864
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-05-26 17:06:26 +0100

    Update module.tf

--- module.tf
+++ module.tf
@@ -4,5 +4,5 @@ resource "spacelift_module" "k8s-module" {
   administrative     = true
   branch             = "master"
   description        = "Infra terraform module"
-  repository         = "terraform-super-module"
+  repository         = "tftest"
 }
