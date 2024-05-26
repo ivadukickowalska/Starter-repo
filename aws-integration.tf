0b42009dafc3ef6dc4a55f853565cc24c5ad25bd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-05-26 20:21:26 +0100

    Update aws-integration.tf

--- aws-integration.tf
+++ aws-integration.tf
@@ -1,6 +1,6 @@
 resource "spacelift_aws_integration" "this" {
   name                          = var.role_name
   role_arn                      = var.role_arn
-  space_id                      = "root"
+  space_id = spacelift_space.starter-repo.id
   generate_credentials_in_worker = false
 }
