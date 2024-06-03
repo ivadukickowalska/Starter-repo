288ccfe791d73520d6c01871562a87d91e50acdd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-06-03 11:55:51 +0100

    Create policies.tf

---
+++ policies.tf
@@ -0,0 +1,64 @@
+# PLAN POLICY
+#
+# This example plan policy prevents you from creating weak passwords, and warns 
+# you when passwords are meh.
+#
+# You can read more about plan policies here:
+#
+# https://docs.spacelift.io/concepts/policy/terraform-plan-policy
+resource "spacelift_policy" "plan" {
+  type = "PLAN"
+  space_id = spacelift_space.starter-repo.id
+  name = "Enforce password strength"
+  body = file("${path.module}/policies/plan.rego")
+}
+
+# Plan policies only take effect when attached to the stack.
+resource "spacelift_policy_attachment" "plan" {
+  policy_id = spacelift_policy.plan.id
+  stack_id  = spacelift_stack.managed.id
+}
+
+# PUSH POLICY
+#
+# This example Git push policy ignores all changes that are outside a project's
+# root. Other than that, it follows the defaults - pushes to the tracked branch
+# trigger tracked runs, pushes to all other branches trigger proposed runs, tag
+# pushes are ignored.
+#
+# You can read more about push policies here:
+#
+# https://docs.spacelift.io/concepts/policy/git-push-policy
+resource "spacelift_policy" "push" {
+  type = "GIT_PUSH"
+  space_id = spacelift_space.starter-repo.id
+  name = "Ignore commits outside the project root"
+  body = file("${path.module}/policies/push.rego")
+}
+
+# Push policies only take effect when attached to the stack.
+resource "spacelift_policy_attachment" "push" {
+  policy_id = spacelift_policy.push.id
+  stack_id  = spacelift_stack.managed.id
+}
+
+# TRIGGER POLICY
+#
+# This example trigger policy will cause every stack that declares dependency on
+# the current one to get triggered the current one is successfully updated.
+#
+# You can read more about trigger policies here:
+#
+# https://docs.spacelift.io/concepts/policy/trigger-policy
+resource "spacelift_policy" "trigger" {
+  type = "TRIGGER"
+  space_id = spacelift_space.starter-repo.id
+  name = "Trigger stacks that declare a dependency explicitly"
+  body = file("${path.module}/policies/trigger.rego")
+}
+
+# Trigger policies only take effect when attached to the stack.
+resource "spacelift_policy_attachment" "trigger" {
+  policy_id = spacelift_policy.trigger.id
+  stack_id  = spacelift_stack.managed.id
+}
